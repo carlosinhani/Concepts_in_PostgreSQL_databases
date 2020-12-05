24f89ea24dce7f80c72238197b04de06fae9d7fb
Carlos.Inhani <carlos.inhani@gmail.com>
2020-12-05 10:08:13 -0300

    view_data_base

---
+++ SQL/view/dados.view.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS funcionarios (
+    id SERIAL,
+	nome VARCHAR(50),
+	gerente INTEGER,
+	PRIMARY KEY (id),
+	FOREIGN KEY (gerente) REFERENCES funcionarios(id)
+);
+
+INSERT INTO funcionarios (nome, gerente) VALUES ('João Paulo',null);
+INSERT INTO funcionarios (nome, gerente) VALUES ('Betriz',1);
+INSERT INTO funcionarios (nome, gerente) VALUES ('Priscila',1);
+INSERT INTO funcionarios (nome, gerente) VALUES ('Daiane',2);
+INSERT INTO funcionarios (nome, gerente) VALUES ('Luiz Eduardo',4);
+
+SELECT id, nome, gerente FROM funcionarios WHERE gerente IS NULL
+UNION ALL
+SELECT id, nome, gerente FROM funcionarios WHERE id = 999; -- APENAS PARA EXEMPLIFICAR
+
+CREATE OR REPLACE RECURSIVE VIEW vw_func(id, gerente, funcionarios) AS (
+    SELECT id, gerente, nome
+	FROM funcionarios
+	WHERE gerente IS NULL
+	
+	UNION ALL
+	
+	SELECT funcionarios.id, funcionarios.gerente, funcionarios.nome
+	FROM funcionarios 
+	JOIN vw_func ON vw_func.id = funcionarios.gerente
+);
+
+SELECT id, gerente, funcionarios FROM vw_func;
+
+
+
+
+
---
+++ SQL/view/view.sql
@@ -0,0 +1,61 @@
+SELECT numero, nome, ativo FROM banco;
+
+CREATE OR REPLACE VIEW vw_bancos AS (
+    SELECT numero, nome, ativo
+    FROM banco
+);
+
+SELECT numero, nome, ativo FROM vw_bancos;
+
+CREATE OR REPLACE VIEW vw_bancos_2 (banco_numero, banco_nome, banco_ativo) AS (
+    SELECT numero, nome, ativo
+    FROM banco
+);
+SELECT banco_numero, banco_nome, banco_ativo FROM vw_bancos_2;
+
+INSERT INTO vw_bancos_2 (banco_numero, banco_nome, banco_ativo)
+VALUES (53, 'Banco Inhani', TRUE);
+
+SELECT banco_numero, banco_nome, banco_ativo FROM vw_bancos_2 WHERE banco_numero = 53;
+SELECT numero, nome, ativo FROM banco WHERE numero = 53;
+
+UPDATE vw_bancos_2 SET banco_ativo = FALSE WHERE banco_numero = 53;
+
+DELETE FROM vw_bancos_2 WHERE banco_numero = 53;
+
+CREATE OR  REPLACE TEMPORARY VIEW vw_agencia AS (
+    SELECT nome FROM agencia
+);
+
+SELECT nome FROM vw_agencia;
+
+CREATE OR REPLACE VIEW vw_bancos_ativos AS (
+    SELECT numero, nome, ativo
+    FROM banco
+    WHERE ativo IS TRUE
+);
+
+INSERT INTO vw_bancos_ativos (numero, nome, ativo) VALUES (53,'Banco Inhani', FALSE);
+
+CREATE OR REPLACE VIEW vw_bancos_com_a AS (
+    SELECT numero, nome, ativo
+    FROM vw_bancos_ativos
+    WHERE nome ILIKE 'a%'
+) WITH CASCADED CHECK OPTION;
+
+SELECT numero, nome, ativo FROM vw_bancos_com_a;
+
+INSERT INTO vw_bancos_com_a (numero, nome, ativo) VALUES (333, 'Alfa Omega',TRUE);
+INSERT INTO vw_bancos_com_a (numero, nome, ativo) VALUES (331, 'Alfa Gama',FALSE);
+INSERT INTO vw_bancos_com_a (numero, nome, ativo) VALUES (332, 'Alfa Gama Beta',FALSE);
+
+
+
+
+
+
+
+
+
+
+
